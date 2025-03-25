b7403f0bd823557860fcc402b9d0d1fa728804c4
Deltunes <elijahfrain@yahoo.com>
2025-03-24 20:19:58 -0500

    some shit

---
+++ characterMover.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+func _physics_process(_delta):
+	var direction = Input.get_vector("left", "right", "up", "down").normalized()
+	var speed = 250
+	velocity = direction * speed
+
+	if move_and_slide():
+		resolve_collisions()
+		
+func resolve_collisions():
+	for i in get_slide_collision_count():
+		var collision := get_slide_collision(i)
+		var body := collision.get_collider() as RigidBody2D
+		if body:
+			body.apply_force(-500 * collision.get_normal())
