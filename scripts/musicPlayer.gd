ef425ab0df5176d4b3a580609b8fcf5ffe64db4a
Deltunes <elijahfrain@yahoo.com>
2025-04-06 21:50:03 -0500

    fixed music player issue

--- scripts/musicPlayer.gd
+++ scripts/musicPlayer.gd
@@ -4,10 +4,8 @@ extends AudioStreamPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	stream = load("res://Audio/GameJamFightTheme.wav")
-	autoplay = true
-	play()
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(_delta):
+	if ($".".playing == false):
+		play()
